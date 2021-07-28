c786b11569971cdc60c7aacdf5bf1961c34da1b4
kiranmaidev <59120603+kiranmaidev@users.noreply.github.com>
2021-07-28 13:48:47 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, Riya");
     System.out.println("Hello, Nina");
+    System.out.println("Hello, Riddhi");
+    System.out.println("Hello, Kuku");
   }
 }
